b1b2c4aae54ee273da444bb38db62534c2b65ca0
HaAbdullah <101064862+HaAbdullah@users.noreply.github.com>
2022-04-06 10:48:31 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,83 +1,46 @@
-class CarManager:
-    """A class responsible for keeping track of all cars in the system.
-    """
+"""Assignment 3 TESTS.
+Warning: This is an extremely incomplete set of tests!
+Add your own to practice writing tests,
+and to be confident your code is correct.
+"""
 
-    # === Private Attributes ===
-    # @type _cars: dict[str, Car]
-    # A map of unique string identifiers to the corresponding cars.
+import unittest
+from super_car import CarManager
 
-    def __init__(self):
-        """Create a new CarManager.
+class TestCar(unittest.TestCase):
 
-        Initially there are no cars in the system.
+  def setUp(self):
+    self.manager = CarManager()
+    self.manager.add_car('car1', 2)
+    self.manager.add_car('car2', 10)
+    self.manager.add_car('car3', 20)
+    
+  def test_initial_fuel(self):
+    self.assertEqual(self.manager.get_car_fuel('car1'), 2)
+    self.assertEqual(self.manager.get_car_fuel('car2'), 10)
+    self.assertEqual(self.manager.get_car_fuel('car3'), 20)
+  
+  def test_initial_pos(self):
+    pos = self.manager.get_car_position('car1')
+    self.assertEqual(pos, (0, 0))
+  
+  def test_move_simple(self):
+    self.manager.move_car('car2', 2, 3)
+    pos = self.manager.get_car_position('car2')
+    self.assertEqual(pos, (2, 3))
+    self.assertEqual(self.manager.get_car_fuel('car2'), 5)
+    
+  def test_move_just_enough(self):
+    self.manager.move_car('car1', 0, 2)
+    pos = self.manager.get_car_position('car1')
+    self.assertEqual(pos, (0, 2))
+    self.assertEqual(self.manager.get_car_fuel('car1'), 0)
+  
+  def test_move_not_enough(self):
+    self.manager.move_car('car1', 3, 5)
+    pos = self.manager.get_car_position('car1')
+    self.assertEqual(pos, (0, 0))
+    self.assertEqual(self.manager.get_car_fuel('car1'), 2)
 
-        @type self: CarManager
-        @rtype: None
-        """
-        self._cars = {}
-
-    def add_car(self, id, fuel):
-        """Add a new car to the system.
-
-        The new car is identified by the string <id>, and has initial amount
-        of fuel <fuel>.
-
-        Do nothing if there is already a car with the given id.
-
-        @type self: CarManager
-        @type id: str
-        @type fuel: int
-        @rtype: None
-        """
-        # Check to make sure the identifier isn't already used.
-        if id not in self._cars:
-            # TODO: Add the new car here.
-            pass
-
-    def move_car(self, id, new_x, new_y):
-        """Move a car in the system.
-
-        The car called <id> should be moved to position (<new_x>, <new_y>).
-
-        @type self: CarManager
-        @type id: str
-        @type new_x: int
-        @type new_y: int
-        @rtype: None
-        """
-        if id in self._cars:
-            # TODO: Move the car.
-            pass
-
-    def get_car_position(self, id):
-        """Return the position of car <id> in the system.
-
-        Return a tuple of the (x, y) position of the car.
-
-        @type self: CarManager
-        @type id: str
-        @rtype: (int, int)
-        """
-        if id in self._cars:
-            # TODO: Get the position of the car.
-            pass
-
-    def get_car_fuel(self, id):
-        """Return the amount of fuel of car <id> in the system.
-
-        @type self: CarManager
-        @type id: str
-        @rtype: int
-        """
-        if id in self._cars:
-            # TODO: Get the amount of fuel of the car.
-            pass
-
-
-class Car:
-    """A car in the Super system.
-
-    Fill in the public or private attributes for this class!
-    """
-    # TODO: design and implement this class.
-    pass
+if __name__ == '__main__':
+  unittest.main()
